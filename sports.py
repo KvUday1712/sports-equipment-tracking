0efde1a9a24b14098f4a8ec67c90dc5f412d7591
uday kv <kvuday70@gmail.com>
2025-02-25 21:09:15 +0530

    Update sports.py

--- sports.py
+++ sports.py
@@ -1,72 +1,77 @@
-#sports equipment tracker
 import datetime
+
 def sportdata():
     while True:
-        l=[101,102,103,104,105]
+        l = [101, 102, 103, 104, 105]
         print("1.101\n2.102\n3.103\n4.104\n5.105\n")
-        a=int(input("equipment id"))
-        if l[a] in l:
-            print("equipment found=")
-            print(l[a])
+        a = int(input("Enter equipment ID: "))
+        
+        if a in l:
+            print("Equipment found:", a)
         else:
-            print("no equipments are in list")
-        print("equipments avalible")
-        print("1.bat\n2.ball\n3.basket ball\n4.foot ball\n5.volley ball\n6.tennnis\n7.racket\n8.cock\n")
-        while True:
-            n=int(input())
-            s={1:'bat',
-               2:'ball',
-               3:'basket ball',
-               4:'foot ball',
-               5:'volley ball',
-               6:'tennnis',
-               7:'racket',
-               8:'cock'
-               }
-            if n in s:
-                print("equipment found")
-                print(s[n])
-            else:
-                print("equipment not found")
-            print("condition of equipment items")
-            print("1.good\n2.average\n3.bad")
-            t=('good','average','bad') 
-            b=int(input("equipment condition is"))
-            if t[b] in t:
-                print("show condition of equipment")
-                print(t[b])
-            else:
-                print("equipment not found")
-            print("1.enter name\n2.enter usn\n3.branch\n4.eid\n5.time for borrowing\n6.time of return")
-            n=input("enter name")
-            u=input("enter usn")
-            b=input("enter branch")
-            eid=int(input("equipment id"))
-            tob_current_datetime = datetime.datetime.now()
-            print("Current date and time:",tob_current_datetime)
-            print("name is=",n)
-            print("usn is=",u)
-            print("branch is=",b)
-            print("eid is=",eid)
-            tor_current_datetime = datetime.datetime.now()
-            print("Current date and time:",tor_current_datetime) 
-            
-            l=int(input("enter same eid"))
-            if l==eid:
-                print("equipment returned")
-            else:
-                print("equipment is not returned so return it")
-            return n,u,b,eid,tob_current_datetime  
-sportdata() 
-
-
-    
-    
-     
+            print("No equipment found in the list.")
         
-        
-        
-  
-    
+        print("\nEquipments available:")
+        print("1.Bat\n2.Ball\n3.Basketball\n4.Football\n5.Volleyball\n6.Tennis\n7.Racket\n8.Cock\n")
 
-            
+        s = {
+            1: 'Bat',
+            2: 'Ball',
+            3: 'Basketball',
+            4: 'Football',
+            5: 'Volleyball',
+            6: 'Tennis',
+            7: 'Racket',
+            8: 'Cock'
+        }
+        
+        n = int(input("Enter the equipment number: "))
+        if n in s:
+            print("Equipment found:", s[n])
+        else:
+            print("Equipment not found.")
+        
+        print("\nCondition of equipment items:")
+        print("1.Good\n2.Average\n3.Bad")
+        
+        t = ['Good', 'Average', 'Bad']
+        b = int(input("Enter equipment condition (1-3): "))
+        
+        if 1 <= b <= 3:
+            print("Condition of equipment:", t[b - 1])
+        else:
+            print("Invalid condition input.")
+        
+        print("\nEnter Borrower Details:")
+        n = input("Enter Name: ")
+        u = input("Enter USN: ")
+        b = input("Enter Branch: ")
+        eid = int(input("Enter Equipment ID: "))
+        
+        tob_current_datetime = datetime.datetime.now()
+        print("\nCurrent date and time:", tob_current_datetime)
+        print("Name:", n)
+        print("USN:", u)
+        print("Branch:", b)
+        print("Equipment ID:", eid)
+
+        # Set return date (borrow date + 7 days)
+        date_of_return = tob_current_datetime + datetime.timedelta(days=7)
+        print("Expected Date of Return:", date_of_return.strftime("%Y-%m-%d"))
+
+        tor_current_datetime = datetime.datetime.now()
+        print("Time of borrowing:", tor_current_datetime)
+
+        # Returning equipment
+        l = int(input("\nEnter the same Equipment ID for returning: "))
+        if l == eid:
+            tor_return_datetime = datetime.datetime.now()
+            print("\nEquipment returned successfully!")
+            print("Returned Date and Time:", tor_return_datetime)
+            print("Expected Date of Return:", date_of_return.strftime("%Y-%m-%d"))
+        else:
+            print("Equipment not returned, please return it.")
+
+        return n, u, b, eid, tob_current_datetime, date_of_return
+
+sportdata()
